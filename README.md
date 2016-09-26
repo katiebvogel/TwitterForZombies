c4afec9648ccbdba38ceebb234b21a1cf36e93b6
Katie Vogel <katiebvogel@gmail.com>
2016-09-26 17:41:59 -0500

    updated migrations and README

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This is a basic Rails App that is modeled on Twitter with a theme of Zombies (based on a Code School Tutorial )
 
-Things you may want to cover:
-
-* Ruby version
+* Ruby version 2.3.1
 
 * System dependencies
 
@@ -20,5 +17,3 @@ Things you may want to cover:
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
-
-* ...
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926222240) do
+ActiveRecord::Schema.define(version: 20160926224107) do
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
-    t.integer  "age"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "email"
     t.boolean  "rotting",    default: false
+    t.integer  "age"
   end
 
 end
